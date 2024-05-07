b9709a83a3641a57aa836ed1c09b1860b39b909c
harcherole <georfprince@gmail.com>
2024-05-07 14:56:24 +0200

    modification files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
             <div className="homeContent">
                 <div className="content">
                     <h1>MIGUIAMA BAMBA Georf</h1>
-                    <h2>Data Scientist</h2>
+                    <h2>Senior Data Scientist</h2>
                     <div className="pdf">
                         <a href={CV} target="_blank">Télécharger mon CV</a>
                     </div>
